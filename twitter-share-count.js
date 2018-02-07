69cdbac78c43457c0f7c91aa372e8aa51ee58895
buggy1985 <peter.bagi@gmail.com>
2018-02-07 13:03:42 +0000

    Initial release

---
+++ twitter-share-count.js
@@ -0,0 +1,51 @@
+var Twitter = require('twitter');
+
+var truncateTweetObj = function(t) {
+    return "["+ t.id+ " | "+t.id_str+"]  "+t.user.screen_name+":  "+t.text;
+}
+
+var extractMaxId = function(url) {
+    return url.split("max_id=")[1].split("&")[0];
+}
+
+var twitterSearch = function(config, query, max_id) {
+    return new Promise(function(resolve,reject){
+        var params = { q: query, count: 100 };
+        if (max_id) params.max_id = max_id;
+        
+        var client = new Twitter(config);
+        client.get('search/tweets', params, function(error, tweets, response) {
+            var body = JSON.parse(response.body);
+            if (error || body.errors) {
+                reject(error ? error : body.errors);
+            } else {
+                var tweetsList = tweets.statuses.map(truncateTweetObj);
+                if ( tweets.statuses.length < params.count ) {
+                    resolve({ tweets: tweetsList });
+                } else {
+                    var next_max_id = extractMaxId(tweets.search_metadata.next_results);
+                    if (max_id !== next_max_id) {
+                        twitterShareCount(config, query, next_max_id).then(function(response) {
+                            resolve({ tweets: tweetsList.concat(response.tweets) });
+                        }, function(e) { 
+                            resolve( { tweets: tweetsList })
+                        });
+                    } else {
+                        resolve({ tweets: tweetsList })
+                    }
+                }
+            }
+        });
+    });
+}
+var twitterShareCount = function(config, query, max_id) {
+    return new Promise(function(resolve,reject){
+        twitterSearch(config, query, max_id).then(function(response) {
+            resolve({shares: response.tweets.length});
+        }, function(e){
+            reject(e);
+        } );
+    });
+}
+
+module.exports = twitterShareCount;
